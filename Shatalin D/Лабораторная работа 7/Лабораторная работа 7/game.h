53946953235020ca5882fd7797557df79f459d71
Dmitrii-Shatalin <62507996+Dmitrii-Shatalin@users.noreply.github.com>
2020-06-03 20:27:35 +0300

    Морской бой

---
+++ Shatalin D/Лабораторная работа 7/Лабораторная работа 7/game.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Player.h"
+
+using namespace std;
+
+class Game {
+	Player* p;
+public:
+	Game();
+	int play();
+	void dispatcher();
+};
+
---
+++ Shatalin D/Лабораторная работа 7/Лабораторная работа 7/Лабораторная работа 7.cpp
@@ -0,0 +1,13 @@
+﻿// Лабораторная работа 7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "Game.h"
+#include <time.h>
+
+using namespace std;
+
+int main() {
+	srand(time(NULL));
+	Game g;
+	g.dispatcher();
+}
